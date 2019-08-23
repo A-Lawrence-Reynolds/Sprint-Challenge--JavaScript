037ab5ecf4649e73255f9074d7c8a6d1d9f0041c
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-08-23 15:04:03 -0700

    met mvp

--- challenges/functions.js
+++ challenges/functions.js
@@ -15,25 +15,24 @@ function consume(a, b, cb) {
  * Create a function named multiply that returns the product of two numbers 
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
-function add(a, b, ) {
-  return a + b;
+function add(num1, num2) {
+  console.log(num1 + num2);
 }
 
-function multiply(a, b) {
-  return a * b;
+function multiply(num1, num2) {
+  console.log(num1 * num2)
 }
 
-function greeting(a, b) {
-  return `Hello ${a} ${b}, nice to meet you`;
+function greeting(first_name, last_name) {
+  console.log(`Hello ${first_name},${last_name} nice to meet you`)
 }
 
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-console.log(consume(2, 2, add)); // 4
-console.log(consume(10, 16, multiply)); // 160
-console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
+consume(2, 2, add); // 4
+consume(10, 16, multiply); // 160
+consume('Mary', 'Poppins', greeting); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ==== 
 
